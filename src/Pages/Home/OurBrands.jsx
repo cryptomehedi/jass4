755a2124d257548d1fc387b790893100ecfb5872
cryptomehedi <classicalmehedi@gmail.com>
2024-01-18 10:15:22 +0600

    ourbrands text added and logo fixed

--- src/Pages/Home/OurBrands.jsx
+++ src/Pages/Home/OurBrands.jsx
@@ -11,42 +11,49 @@ import brand3 from './../../assets/brand-2.png'
 const OurBrands = () => {
     return (
 
-        <div className="h-full border-transparent border mt-10 my-auto">
-            <div className="mt-10 p-11 md:px-60">
-                <Swiper
-                    watchSlidesProgress={true}
-                    slidesPerView={3}
-                    className="mySwiper w-full"
-                    spaceBetween={30}
-                    autoplay={{
-                        delay: 2500,
-                        disableOnInteraction: false,
-                    }}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    breakpoints={{
-                        200: {
-                        slidesPerView: 1,
-                        spaceBetween: 20,
-                        centeredSlides:true
-                        },
-                        768: {
-                        slidesPerView: 2,
-                        spaceBetween: 40,
-                        },
-                        1024: {
-                        slidesPerView: 3,
-                        spaceBetween: 50,
-                        },
-                    }}
-                    modules={[ Autoplay, Pagination, Navigation]}
-                >
-                    <SwiperSlide><img src={brand1} alt="" /></SwiperSlide>
-                    <SwiperSlide><img src={brand2} alt="" /></SwiperSlide>
-                    <SwiperSlide><img src={brand3} alt="" /></SwiperSlide>
-                </Swiper>
-               
+        <div>
+            
+            <div className="h-full border-transparent border mt-10 my-auto">
+                    <div className="mt-10 text-center text-3xl font-thin text-orange-400 uppercase">
+                        Our <span className="font-semibold">Brands</span>
+                    </div>
+                <div className="p-11 md:px-60">
+                    
+                    <Swiper
+                        watchSlidesProgress={true}
+                        slidesPerView={3}
+                        className="mySwiper w-full"
+                        spaceBetween={30}
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false,
+                        }}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        breakpoints={{
+                            200: {
+                            slidesPerView: 1,
+                            spaceBetween: 20,
+                            centeredSlides:true
+                            },
+                            768: {
+                            slidesPerView: 2,
+                            spaceBetween: 40,
+                            },
+                            1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 50,
+                            },
+                        }}
+                        modules={[ Autoplay, Pagination, Navigation]}
+                    >
+                        <SwiperSlide><img src={brand1} alt="" /></SwiperSlide>
+                        <SwiperSlide><img src={brand2} alt="" /></SwiperSlide>
+                        <SwiperSlide><img src={brand3} alt="" /></SwiperSlide>
+                    </Swiper>
+                
+                </div>
             </div>
         </div>
         
